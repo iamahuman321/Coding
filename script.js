d33f56f2b525cc990debd5b2bdb75e095d5a13d1
Sigma male <154803612+iamahuman321@users.noreply.github.com>
2024-02-25 19:05:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#element', {
+    strings: ['Discover the wonders of nature', 'Explore diverse biomes', 'Protect our planet'],
+    typeSpeed: 60,
+});
+
+const contactButtons = document.querySelectorAll('.contact-button');
+
+contactButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const email = button.getAttribute('data-email');
+        window.open(`mailto:${email}`, '_blank');
+    });
+});
